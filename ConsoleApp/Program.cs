5696560efd6a0f00de023dca7bf1f7b782a0f97f
Frank Witte <frank@franksworld.nl>
2019-11-13 22:03:34 +0100

    init

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,20 +7,18 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-//            var source = "meilenstein";
-//            var target = "levenshtein";
 //            var source = "saturday";
 //            var target = "sunday";
-//            var source = "kitten";
-//            var target = "sitting";
-            var source = "drank";
-            var target = "dr?nk";
+            var source = "meilenstein";
+            var target = "levenshtein";
 
             var calculator = new LevenshteinCalculator();
             var result = calculator.Calculate(source, target);
-            
-            Console.WriteLine($"The distance is {result.Distance}.");
-            Console.WriteLine("Result:");
+
+            Console.WriteLine($"The distance between '{source}' and '{target}' is {result.Distance}.");
+            Console.WriteLine();
+            Console.WriteLine("Path:");
+            Console.WriteLine();
             Console.WriteLine(result);
         }
     }
